bb87b0ba900127b8e38b9f945b1d0ac913215afe
ariannagreco3393 <a.greco3393@gmail.com>
2022-04-12 12:06:53 +0200

    add versione lunga e abbreviata

--- js-snack-3/main.js
+++ js-snack-3/main.js
@@ -2,6 +2,18 @@
 Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 */
 
+
+
+function reverse(string) {
+    const splString = string.split("")
+    const revString = splString.reverse()
+    console.log(revString);
+    return revString.join("")
+}
+console.log(reverse('ciao'));
+
+//versione abbreviata
+
 function oppositeWord(string) {
     return string.split("").reverse().join("")
 }
